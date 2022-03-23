1ad1596bed847668a52c4fe11abd4dd9d7c3ad68
prasadbhanu703 <prasadbhanu703@gmail.com>
2022-03-23 20:44:41 +0530

    changed widget url

--- src/Landkit.js
+++ src/Landkit.js
@@ -5,8 +5,8 @@ import Part3 from './Part3';
 const Landkit = () => {
     return (
     <div className="main-div">
-        <iframe style={{height:"600px", width:"400px", border : "none"}} title="relay-widget"
-        src='https://app.relaychain.com/#/cross-chain-bridge-transfer?widget'></iframe>
+        <iframe style={{height:"700px", width:"400px", border : "none"}} title="relay-widget"
+        src='https://development-branch.relaychain.com/#/cross-chain-bridge-transfer?widget'></iframe>
     <Part1 />
     <Part2 />
     <Part3 />
